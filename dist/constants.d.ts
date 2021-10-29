5377ab08cf319beda35ce06b6428bd4bc6e6e99d
Andriy Noha <anoha@vodworks.com>
2021-10-29 15:56:43 +0300

    rebuild, increase version

--- dist/constants.d.ts
+++ dist/constants.d.ts
@@ -1,9 +1,9 @@
-import JSBI from 'jsbi'
-export declare const FACTORY_ADDRESS = '0x01a2aB72dd3A49700CFea8b87e9E6ba7Dfb64809'
-export declare const INIT_CODE_HASH = '0xa9595a22cdacdc76f8b097ef7488af7895090c30f5f3d18d798e12fcda69cf7d'
-export declare const MINIMUM_LIQUIDITY: JSBI
-export declare const ZERO: JSBI
-export declare const ONE: JSBI
-export declare const FIVE: JSBI
-export declare const _997: JSBI
-export declare const _1000: JSBI
+import JSBI from 'jsbi';
+export declare const FACTORY_ADDRESS = "0x11AB0Ca40B2E9Bf2c98718539cA1aD7486999E57";
+export declare const INIT_CODE_HASH = "0x0afcd21f90e27818df9c484881aac8a20b869cbd86156c655d2020ef6950a5ba";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
