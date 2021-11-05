d3e5546a040bb56837980deeeffb3e12f91aad9e
Lint Action <lint-action@samuelmeuli.com>
2021-11-05 10:44:55 +0000

    Fix code style issues with Prettier

--- dist/constants.d.ts
+++ dist/constants.d.ts
@@ -1,9 +1,9 @@
-import JSBI from 'jsbi';
-export declare const FACTORY_ADDRESS = "0xe3DcF89D0c90A877cD82283EdFA7C3Bd03e77E86";
-export declare const INIT_CODE_HASH = "0x0afcd21f90e27818df9c484881aac8a20b869cbd86156c655d2020ef6950a5ba";
-export declare const MINIMUM_LIQUIDITY: JSBI;
-export declare const ZERO: JSBI;
-export declare const ONE: JSBI;
-export declare const FIVE: JSBI;
-export declare const _997: JSBI;
-export declare const _1000: JSBI;
+import JSBI from 'jsbi'
+export declare const FACTORY_ADDRESS = '0xe3DcF89D0c90A877cD82283EdFA7C3Bd03e77E86'
+export declare const INIT_CODE_HASH = '0x0afcd21f90e27818df9c484881aac8a20b869cbd86156c655d2020ef6950a5ba'
+export declare const MINIMUM_LIQUIDITY: JSBI
+export declare const ZERO: JSBI
+export declare const ONE: JSBI
+export declare const FIVE: JSBI
+export declare const _997: JSBI
+export declare const _1000: JSBI
