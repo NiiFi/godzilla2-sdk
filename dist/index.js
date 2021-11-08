38fe8ee48f8d56dcc21e14c82f79e7cc797255d6
r-senchuk <frspm.roman@gmail.com>
2021-11-08 12:17:49 +0200

    testing the removal of dist

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./godzilla2-sdk.cjs.production.min.js')
-} else {
-  module.exports = require('./godzilla2-sdk.cjs.development.js')
-}
